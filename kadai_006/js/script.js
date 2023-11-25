14273fffd2c9460bd61309e84da0bbf0b31c2d8e
jumpei1107 <142232353+jumpei1107@users.noreply.github.com>
2023-11-25 12:30:48 +0900

    Add files via upload

---
+++ kadai_006/js/script.js
@@ -0,0 +1,15 @@
+
+// $(function(){
+//     $("button").on("click",function(){
+//         alert("クリックされました。");
+//     })
+// })
+
+
+
+
+$(function(){
+  $("h1").on("mouseenter",function(){
+    $("#target").addClass("heading");
+  })
+})
